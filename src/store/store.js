2dfab49618c31624a3647eea6c78634f8b8f4072
Jose Akle <deltaalpha94@hotmail.com>
2023-01-02 13:55:13 -0500

    Add main silce reducer to the store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import searchTermReducer from './searchTermSlice';
-import subredditsReducer from './subredditsSlice'
+import subredditsReducer from './subredditsSlice';
+import mainReducer from './mainSlice';
 
 export const store = configureStore({
   reducer: {
     searchTerm: searchTermReducer,
-    subreddits: subredditsReducer
+    subreddits: subredditsReducer,
+    main: mainReducer
   }
 })
