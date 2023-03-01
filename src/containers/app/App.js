a4496e53f1a51ada5ad689c6391fcc66d8aff960
Jose Akle <deltaalpha94@hotmail.com>
2023-03-01 15:07:38 -0500

    Make the App component return the All component nested in a Router component

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,22 +1,14 @@
 import './App.css';
 import React from 'react';
-import { Aside } from '../aside/Aside';
-import { Header } from '../header/Header';
-import { Main } from '../main/Main';
+import { All } from '../all/All';
+import {BrowserRouter as Router} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div>
-      <header>
-        <Header />
-      </header>
-      <aside>
-        <Aside />
-      </aside>
-      <main>
-        <Main />
-      </main>
-    </div>
+    <Router>
+      <All />
+    </Router>
   );
 }
 
