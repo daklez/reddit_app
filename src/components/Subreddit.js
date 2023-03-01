496722f2efaaf325e41777a137be8a02e22be651
Jose Akle <deltaalpha94@hotmail.com>
2023-03-01 13:51:58 -0500

    Add a dynamic class to <li>

--- src/components/Subreddit.js
+++ src/components/Subreddit.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectPickedSubreddit } from '../store/mainSlice';
 
 export const Subreddit = (props) => {
   const { url, name, avatar, id, handleClick } = props
+  const pickedSubreddit = useSelector(selectPickedSubreddit)
+  const selected = pickedSubreddit.includes(url)
 
   return (
-    <li key={id} id={url} onClick={handleClick}>
+    <li key={id} id={url} onClick={handleClick} className={selected ? 'subreddit active':'subreddit'}>
       <img
         src={
           avatar || "https://cdn-icons-png.flaticon.com/512/3670/3670226.png"
