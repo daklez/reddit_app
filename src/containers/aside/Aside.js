76494f2b4db95b560f64394440471e1732d49e7a
Jose Akle <deltaalpha94@hotmail.com>
2023-03-01 15:16:20 -0500

    Add an id to the <div> and  the 'aside' prop to the Subreddits component

--- src/containers/aside/Aside.js
+++ src/containers/aside/Aside.js
@@ -1,11 +1,12 @@
+import './Aside.css';
 import React from "react";
 import { Subreddits } from "../../components/Subreddits";
 
 export const Aside = () => {
   return (
-    <div>
-      <h2>Subreddits</h2>
-      <Subreddits />
+    <div id="aside">
+      <h2>Popular Subreddits</h2>
+      <Subreddits aside={true}/>
     </div>
   )
 }
