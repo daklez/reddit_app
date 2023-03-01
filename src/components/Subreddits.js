21bbe5c66e944e1ead901204151bf1c38f0fa6a8
Jose Akle <deltaalpha94@hotmail.com>
2023-03-01 14:51:37 -0500

    Add the logic to handle the behavior of the header subreddits, scroll to the top, and change the url when selecting a new subreddit

--- src/components/Subreddits.js
+++ src/components/Subreddits.js
@@ -7,27 +7,58 @@ import {
   getSubreddits } from "../store/subredditsSlice";
 import { Subreddit } from "./Subreddit";
 import { pickNewSubrreddit } from "../store/mainSlice";
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import { useHistory, useLocation } from 'react-router-dom';
 
-export const Subreddits = () => {
+export const Subreddits = (props) => {
   const subreddits = useSelector(selectSubreddits);
   const subredditsLoading = useSelector(selectSubredditsLoading);
   const subredditsFailed = useSelector(selectSubredditsFailed);
   const dispatch = useDispatch();
+  const history = useHistory()
+  const location = useLocation()
+  console.log(location.pathname)
 
   useEffect(() => {
     dispatch(getSubreddits())
   }, [dispatch]);
 
   const handleClick = ({target}) => {
-    dispatch(pickNewSubrreddit(target.id))
+    dispatch(pickNewSubrreddit(target.id));
+    window.scrollTo(0, 0)
+    if(!props.aside){
+      document.getElementById('header-subreddits').style.display = 'none';
+      props.setShowSubreddits(!props.showSubreddits)
+    }
+    location.pathname = target.id
+    history.push(target.id)
   }
 
   const handleFailure = () => {
     dispatch(getSubreddits())
   }
 
+  if(!props.aside){
+    if(subredditsFailed){
+      return (
+        <div>
+          <h3>Something went wrong</h3>
+          <button onClick={handleFailure}>Try Again</button>
+        </div>
+      )
+    }
+    return (
+      <ul id='header-subreddits'>
+        {subreddits.map(subreddit => {
+          return <Subreddit url={subreddit.url} name={subreddit.display_name_prefixed} avatar={subreddit.icon_img} id={subreddit.id} handleClick={handleClick} />
+        })}
+      </ul>
+    )
+  }
+
   if(subredditsLoading){
-    return <p>Loading Subreddits...</p>
+    return <Skeleton count={20} height={30}/>
   }
 
   if(subredditsFailed){
