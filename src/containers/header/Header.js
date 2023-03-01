1b724b321582d6d27ae3afccacb415c6ee9af860
Jose Akle <deltaalpha94@hotmail.com>
2023-03-01 15:23:05 -0500

    Add the logic to toggle the visibility of the header subreddits using the icon and add a small version of the logo for mobile devices

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,12 +1,29 @@
 import './header.css'
-import React from "react";
+import React, {useState} from "react";
 import Logo from '../../Logo.png';
+import LogoSmall from '../../LogoSmall.png';
 import { SearchBar } from "../../components/SearchBar";
+import { Subreddits } from '../../components/Subreddits';
 
 export const Header = () => {
+  const [showSubreddits, setShowSubreddits] = useState(false);
+
+  const handleClick = () => {
+    setShowSubreddits(!showSubreddits)
+  }
+ 
   return (
     <div id="header">
-      <img src={Logo} alt="logo" width={50} height={50}/>
+      <div id='logo-container'>
+        <picture>
+          <source media="(max-width: 1000px)" srcSet={LogoSmall} />
+          <img src={Logo} alt="IfItDoesntMatchAnyMedia" />
+        </picture>
+        <span className="material-symbols-outlined" id='header-subreddits-icon' onClick={handleClick}>
+          expand_more
+        </span> 
+        {showSubreddits && <Subreddits aside={false} setShowSubreddits={setShowSubreddits} showSubreddits={showSubreddits}/> }
+      </div>
       <SearchBar />
     </div>
     );
