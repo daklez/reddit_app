cdf7ab6fc6709893415ce1fe91f8d4473a778816
Jose Akle <deltaalpha94@hotmail.com>
2023-03-01 13:55:36 -0500

    Add some <div> elements to improve the layout

--- src/components/SubredditInfo.js
+++ src/components/SubredditInfo.js
@@ -4,15 +4,17 @@ export const SubredditInfo = (props) => {
     const currentSubreddit = props.currentSubreddit;
 
     return (
-        <div>
+        <div id="subreddit-info">
           <img 
             src={currentSubreddit.icon_img || "https://cdn-icons-png.flaticon.com/512/3670/3670226.png"} 
             alt={currentSubreddit.display_name}
             width={128}
             height={128}
           />
-          <h1>{currentSubreddit.title}</h1>
+          <div>
+            <h1>{currentSubreddit.title}</h1>
           <p>{currentSubreddit.display_name_prefixed}</p>
+          </div>   
         </div>
     )
 }
